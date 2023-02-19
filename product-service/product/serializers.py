4f27b41e43b9d4b63f9578a7bcbd1e23147daa90
Daniel Ale <danielale9291@gmail.com>
2023-02-19 10:56:04 +0100

    add user call by id to product

--- product-service/product/serializers.py
+++ product-service/product/serializers.py
@@ -10,11 +10,14 @@ USER_GRPC_SERVER = settings.USER_GRPC_SERVER
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # owner = serializers.SerializerMethodField()
+    owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = '__all__'
+        extra_kwargs = {
+            'owner_id': {'write_only': True}
+        }
 
     def create(self, validated_data):
         owner_id = validated_data.get('owner_id', None)
@@ -25,10 +28,12 @@ class ProductSerializer(serializers.ModelSerializer):
                 print(res, end='')
         return super().create(validated_data)
 
-    # def get_owner(self, instance):
-    #     with grpc.insecure_channel(USER_GRPC_SERVER) as channel:
-    #         stub = user_pb2_grpc.UserServiceStub(channel)
-    #         res = stub.GetUser(user_pb2.UserRetrieveRequest(id=str(instance.owner_id)))
-    #         res = json.dumps(res)
-    #         return res
+    def get_owner(self, instance):
+        owner = {}
+        with grpc.insecure_channel(USER_GRPC_SERVER) as channel:
+            stub = user_pb2_grpc.UserServiceStub(channel)
+            res = stub.GetUser(user_pb2.UserRetrieveRequest(id=str(instance.owner_id)))
+            if res:
+                owner.update({'id': res.id, 'email': res.email, 'firstname': res.firstname, 'lastname': res.lastname})
+        return owner
 
