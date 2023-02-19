2f0748a6e5536713d258dc9b2c18f4d51c26ea1e
Daniel Ale <danielale9291@gmail.com>
2023-02-19 12:24:09 +0100

    cleanup

--- user-service/user/services.py
+++ user-service/user/services.py
@@ -1,9 +1,7 @@
 import grpc
 from google.protobuf import empty_pb2
-from django_grpc_framework.services import Service
 from .models import User
 from .serializers import UserProtoSerializer
-# from .grpc import user_pb2_grpc
 from django_grpc_proto.py_grpc import user_pb2_grpc
 
 
